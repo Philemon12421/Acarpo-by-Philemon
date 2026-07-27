3dc9c940be47e7540f30466420c60de46dd96ce3
Philemon Osei Kusi <philemonkusi292@gmail.com>
2026-07-27 12:51:48 +0000

    Clarify BrowserRouter import in main.jsx
    Updated comments to clarify BrowserRouter usage.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Note: BrowserRouter now lives inside App.jsx itself (wrapping AppShell),
+// so this file doesn't need to import react-router-dom directly.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
